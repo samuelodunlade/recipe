ad72746a8992090612a245be89e1a2466caee5b6
“samuelodunlade” <samodunlade@gmail.com>
2024-09-11 14:27:14 +0100

    First version for recipe app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react"
+import axios from "axios"
 
-function App() {
+
+import Banner from "./Recipe_components/Banner"
+import Copy from "./Recipe_components/Copy"
+import Footer from "./Recipe_components/Footer"
+import List from "./Recipe_components/List"
+import Nav from "./Recipe_components/Nav"
+
+
+const RecipeApp = () => {
+
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(false);
+    const [recipes, setRecipes] = useState([]);
+    const [frecipe, setFRecipe] = useState([]);
+    const[keyword, setKeyWord] = useState('');
+    
+    useEffect(()=>{
+      makeApiCall()
+    }, [])
+
+  //this function will do the filtering
+    const handleFilter = () => {
+      
+      const filtered_recipes = recipes.filter((recipe)=>{
+        return recipe.name.toLowerCase().includes(keyword.toLowerCase())
+      })
+
+      setFRecipe(filtered_recipes)
+    }
+    //make the call and get the data for us
+    function makeApiCall(){
+        axios.get("https://dummyjson.com/recipes")
+        .then(function(resp){
+          console.log(resp.data.recipes)
+          setLoading(false)
+          setRecipes(resp.data.recipes)
+        })
+        .catch(function(err){
+          console.log(err)
+          setLoading(false);
+          setError(true)
+        })
+    }
+
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container-fluid">
+      <Nav/>
+      <Banner setKeyWord={setKeyWord} keyword={keyword} handleFilter={handleFilter}/>
+      <List  loading={loading} error={error} recipes={recipes} frecipe={frecipe} keyword={keyword}/>
+      <Footer/>
+      <Copy/>
     </div>
-  );
+  )
 }
 
-export default App;
+export default RecipeApp
